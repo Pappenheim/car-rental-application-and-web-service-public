42ec328e1398e78bc3f1752c98a845fc1beaa6fb
Pappenheim <34920239+Pappenheim@users.noreply.github.com>
2024-05-10 10:11:32 +0200

    Update karma.config.js

--- karma.config.js
+++ karma.config.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
     base: 'Chrome',
     flags: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox', '--remote-debugging-port=9222']
   }
-}
+},
 
     singleRun: false,
     restartOnFileChange: true
